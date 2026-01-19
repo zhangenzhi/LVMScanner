5a839850d41efd70c7f4ff400360d41b521ef9f7
张恩智 <zhangenzhi@zezzz.local>
2026-01-19 22:56:40 +0900

    Initial Commit

---
+++ LVMScanner/LVMScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LVMScannerApp.swift
+//  LVMScanner
+//
+//  Created by 张恩智 on 2026/1/19.
+//
+
+import SwiftUI
+
+@main
+struct LVMScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
